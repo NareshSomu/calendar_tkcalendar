2db955d1a9fb77c9fc37733c3d0f2f1dd4e2059b
NareshSomu <114392817+NareshSomu@users.noreply.github.com>
2023-07-29 21:02:07 +0530

    Add files via upload

---
+++ project2.py
@@ -0,0 +1,19 @@
+from tkinter import *
+from tkcalendar import *
+
+window=Tk()
+window.title('pick the date')
+window.geometry('500x400')
+window.configure(bg='light blue')
+
+cal=Calendar(window,selectmode='day')
+cal.pack(pady=20)
+
+def grab_date():
+    l1.config(text=cal.get_date(),fg="#a52a2a",bg="#ffe4c4",font=("calibri",20))
+
+b1=Button(window,text='Select the Date',command=grab_date,bg="grey",fg="black",bd=4,font=('Helvetica',20))
+b1.pack(pady=20)
+l1=Label(window,text='',bg='light blue')
+l1.pack(pady=20)
+window.mainloop()

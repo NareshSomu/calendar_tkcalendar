34dc7c3b011894439bc41153f7ae06d3f4fa5799
NareshSomu <114392817+NareshSomu@users.noreply.github.com>
2023-07-29 21:03:47 +0530

    Add files via upload

--- project2.py
+++ project2.py
@@ -7,13 +7,13 @@ window.geometry('500x400')
 window.configure(bg='light blue')
 
 cal=Calendar(window,selectmode='day')
-cal.pack(pady=20)
+cal.pack()
 
 def grab_date():
     l1.config(text=cal.get_date(),fg="#a52a2a",bg="#ffe4c4",font=("calibri",20))
 
 b1=Button(window,text='Select the Date',command=grab_date,bg="grey",fg="black",bd=4,font=('Helvetica',20))
-b1.pack(pady=20)
+b1.pack()
 l1=Label(window,text='',bg='light blue')
-l1.pack(pady=20)
+l1.pack()
 window.mainloop()
